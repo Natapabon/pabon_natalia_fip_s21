e13c91ba5a0869ef65d26dda0bb555d74a436c34
Nata Pabon <ta@MacBook-Air-de-Ta.local>
2021-07-31 05:47:28 -0400

    adding our beer page

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
         burgerCon = document.querySelector("#burgerCon"),
         navLinks = burgerCon.querySelectorAll("a");
 
+        videoPlay = document.querySelector(".bannerVideo")
+        audioPlay = document.querySelector("#playAudio")
+
   //functions go in the middle (our app instructions)
   function showNavigation() {
     //turn the button for some UX microinteractivity
@@ -12,11 +15,28 @@
     burgerCon.classList.toggle("slideToggle");
   }
 
+  //play video function in OUR BEER page
+  function playAudio() {
+    if (videoPlay.muted) {
+      videoPlay.muted = false;
+      audioPlay.textContent = "■ Mute audio"
+    }
+    else {
+      videoPlay.muted = true;
+      audioPlay.textContent = "▶ Play audio"
+    }
+  }
+
+
   //event handling goes at the bottom
   button.addEventListener("click", showNavigation);
 
   for (link of navLinks) {
     link.addEventListener("click", showNavigation);
   }
+
+  //event listener for audioPlay
+  audioPlay.addEventListener("click", playAudio);
+
 
 })();
